8678997c00f35bbedb7ec71ea241779ad4bafcdd
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2025-07-19 14:16:45 -0300

    feat: add SectionOneHome component with image and buttons

---
+++ src/components/section-one-home.jsx
@@ -0,0 +1,46 @@
+import { Button } from "flowbite-react";
+import { Underline } from "../components/underline";
+
+const SectionOneHome = () => {
+  return (
+    <section className="flex gap-20 bg-white">
+      <div>
+        <div className="flex flex-col items-start max-w-[636px]">
+          <h1 className="relative mt-10 text-[66px] font-bold leading-[76px] text-gray-900">
+            Troque o que você
+            <br /> já não usa por o<br />
+            <span className="relative text-blue-600"> que você precisa</span>
+            <Underline />
+          </h1>
+          <p className="max-w-[546px] mt-8 text-3xl leading-[44px] text-gray-600">
+            Encontre comunidades locais para trocar itens sem gastar nada.
+          </p>
+        </div>
+        <div className="flex mt-4 space-x-4">
+          <Button
+            className="w-56 h-16 rounded-2xl text-xl ease duration-200"
+            href="#"
+          >
+            Quero trocar
+          </Button>
+          <Button
+            className="w-56 h-16 rounded-2xl text-xl text-[#FE7A1B] border hover:bg-[#FE7A1B] hover:text-white ease duration-200"
+            color="bg-white"
+            href="#"
+          >
+            Como funciona 
+          </Button>
+        </div>
+      </div>
+      <div>
+        <img
+          src="src/assets/images/image-section-one-home.png"
+          alt="image"
+          className="w-[450px] h-[446px] object-cover"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default SectionOneHome;
