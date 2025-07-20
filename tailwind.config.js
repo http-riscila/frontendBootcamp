eb2cd6a49ab6d778257b34ff8e378fb042deea10
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2025-07-19 23:09:01 -0300

    refactor: Limpeza de dependências e estilos
    - Remoção da dependência flowbite-react e configurações relacionadas dos arquivos package.json e CSS.
    - Consolidação das importações de fontes e estilos de corpo no index.css para melhor organização.
    - Adição de um novo arquivo de configuração CSS do Tailwind para gerenciar as cores do tema e estender a largura máxima.
    - Atualização da configuração do Vite para remover o plugin flowbite-react.

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/* filepath: d:\estudo-programacao\Avanti\projeto\frontendBootcamp\tailwind.config.js */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        medium: "24rem", // 384px
+      },
+      colors: {
+        primary: "var(--color-primary)", // #1b5fff
+        secondary: "var(--color-secondary)", // #fe7a1b
+        tertiary: "var(--color-tertiary)", // #d1dfff
+        title: "var(--color-title)", // #111827
+        text: "var(--color-text)", // #79767d
+        background: "var(--color-background)", // #fff
+        feedback: {
+          error: "var(--color-feedback-error)", // #f87171
+          success: "var(--color-feedback-success)", // #84cc16
+          warning: "var(--color-feedback-warning)", // #facc15
+        },
+      },
+    },
+  },
+  plugins: [],
+};
