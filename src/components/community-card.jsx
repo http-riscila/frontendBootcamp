9a079a8c2437baf5dc9e202a4b57ca9de1aced5b
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2025-07-21 21:48:43 -0300

    refactor(community-card): remover badge de categoria não utilizado e limpar código

--- src/components/community-card.jsx
+++ src/components/community-card.jsx
@@ -1,8 +1,5 @@
-
-
 export default function CommunityCard({
   imageUrl,
-  categoryLabel = "Categoria",
   title,
   description,
   membersLabel = "membros",
@@ -16,7 +13,6 @@ export default function CommunityCard({
     <div
       className={`max-w-medium bg-background flex h-[500px] flex-col overflow-hidden rounded-2xl shadow-sm ${className}`}
     >
-      {/* Imagem */}
       <div className="flex h-[220px] w-full items-center justify-center rounded-t-2xl bg-gray-200">
         {imageUrl ? (
           <img
@@ -30,11 +26,7 @@ export default function CommunityCard({
       </div>
 
       <div className="mt-0 rounded-b-2xl border border-t-0 border-x-[#1b5fff] border-b-[#1b5fff] py-8 pb-10">
-       
         <div className="mb-2 flex items-start justify-between px-10">
-          <span className="flex w-full max-w-[68px] items-center justify-center rounded-3xl border border-blue-100 bg-[#d1dfff] px-2 py-1 text-sm text-[#1b5fff]">
-            {categoryLabel}
-          </span>
           {membersCount !== undefined && (
             <div className="flex gap-1 text-sm text-[#79767d]">
               <img src="src/assets/svgs/user-icon.svg" alt="user" />
@@ -42,13 +34,13 @@ export default function CommunityCard({
             </div>
           )}
         </div>
-      
+
         <div className="flex flex-col items-start px-10">
           <h5 className="text-title mt-2 text-3xl font-bold">{title}</h5>
           <p className="mt-1 mb-4 w-full max-w-[310px] flex-1 text-xl text-[#79767d]">
             {description}
           </p>
-          {/* Botão limpo */}
+
           <button
             className={`mt-3 w-full cursor-pointer rounded-lg px-4 py-3 text-xl font-semibold transition-all duration-100 ${
               isSelected
