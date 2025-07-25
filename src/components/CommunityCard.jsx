289a3e58eceb8af2d9128e8f75719048337c44c0
http-riscila <prigadelha10@gmail.com>
2025-07-24 22:01:35 -0300

    feat: service de contagem de membros

--- src/components/CommunityCard.jsx
+++ src/components/CommunityCard.jsx
@@ -1,16 +1,32 @@
+import { useState, useEffect } from "react";
+import { countMembersByCommunity } from "../services/member-service";
 import memberIcon from "../assets/icons/member-icon.svg";
-export default function CommunityCard({
-  image,
-  title,
-  description,
-  membersCount,
-}) {
+
+export default function CommunityCard({ community }) {
+  const [membersCount, setMembersCount] = useState(0);
+
+  useEffect(() => {
+    async function getMembersCount() {
+      try {
+        const members = await countMembersByCommunity(community?.id);
+        setMembersCount(members);
+      } catch (error) {
+        console.log("Error fetching count data", error);
+      }
+    }
+    getMembersCount();
+  }, []);
+
   return (
     <div className="flex flex-col overflow-hidden rounded-2xl border border-[#1B5FFF] bg-white shadow-sm">
       {/* Imagem da comunidade */}
       <div className="flex h-40 w-full items-center justify-center overflow-hidden bg-gray-200">
-        {image ? (
-          <img src={image} alt={title} className="h-full w-full object-cover" />
+        {community?.imageUrl ? (
+          <img
+            src={community?.imageUrl}
+            alt="Imagem da comunidade"
+            className="h-full w-full object-cover"
+          />
         ) : (
           <span className="text-sm text-gray-400">&nbsp;</span>
         )}
@@ -19,14 +35,14 @@ export default function CommunityCard({
       <div className="flex flex-1 flex-col gap-4 p-6">
         <div className="flex flex-row justify-end gap-2 text-xs text-[var(--color-text)]">
           <img src={memberIcon || 0} />
-          <span className="flex flex-row gap-1">
-            {membersCount} <p>membros</p>
-          </span>
+          <span className="flex flex-row gap-1">{membersCount}</span>
         </div>
         <div>
-          <h5 className="text-xl font-bold text-gray-900">{title}</h5>
+          <h5 className="text-xl font-bold text-gray-900">{community?.name}</h5>
 
-          <p className="flex-1 text-base text-gray-600">{description}</p>
+          <p className="flex-1 text-base text-gray-600">
+            {community?.description}
+          </p>
         </div>
         <button
           className={`w-full cursor-pointer rounded-2xl border-0 bg-[var(--color-primary)] py-3 text-xl font-medium text-white transition-colors duration-700 hover:bg-[var(--color-tertiary)]`}
